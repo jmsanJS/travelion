998c0cda22975f39fdeddc85e65ae60d4cd9ea0d
Juan Sanchez <juanboca90@gmail.com>
2024-11-01 14:20:14 +0100

    Welcome Screen (index)

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,22 @@
-import { StyleSheet, ImageBackground, View, Text } from "react-native";
+import {
+  StyleSheet,
+  ImageBackground,
+  View,
+  Text,
+  Pressable,
+} from "react-native";
 import React from "react";
-
-import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 
 export default function WelcomeScreen() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -11,14 +24,22 @@ export default function WelcomeScreen() {
         resizeMode="cover"
         style={styles.image}
       >
-        <View style={styles.contentContainer}>
-          <Text style={styles.title}>
-            Welcome
-          </Text>
+        <Animated.View
+          entering={FadeInDown}
+          style={styles.contentContainer}
+        >
+          <LinearGradient
+            colors={["rgba(0,0,0,0.8)", "transparent"]}
+            style={styles.background}
+          />
+          <Text style={styles.title}>Welcome</Text>
           <Text style={styles.subtitle}>
-            Explore new breathtaking places with us
+            Ready to explore new breathtaking places with us?
           </Text>
-        </View>
+          <Pressable onPress={() => router.push("/home")} style={styles.btn}>
+            <Text style={styles.btnText}>Let's go!</Text>
+          </Pressable>
+        </Animated.View>
       </ImageBackground>
     </View>
   );
@@ -29,17 +50,49 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   contentContainer: {
-    width: wp(90)
+    width: wp(100),
+    height: hp(35),
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 200,
   },
   title: {
-    color: "white",
+    color: "#fff",
+    fontSize: hp(5.5),
+    fontWeight: "200",
+    letterSpacing: 2,
   },
   subtitle: {
-    color: "white",
+    color: "#fff",
+    width: wp(85),
+    fontSize: hp(2.2),
+    fontWeight: "300",
+    letterSpacing: 1,
+    textAlign: "center",
   },
   image: {
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
+  },
+  btn: {
+    backgroundColor: "#000",
+    width: wp(50),
+    borderRadius: 50,
+    alignSelf: "center",
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: hp(2.5),
+    fontWeight: "500",
+    textAlign: "center",
+    padding: hp(2),
+    letterSpacing: 1,
   },
 });
