c660b72a4597eb512bdd8c53a6f3abe3d448d6d4
Juan Sanchez <juanboca90@gmail.com>
2024-11-04 17:18:08 +0100

    List of categories and images refs

---
+++ constants/constants.ts
@@ -0,0 +1,48 @@
+export const categoriesList = ["All", "Popular", "Featured", "More"];
+
+export const categoriesImg = [
+  {
+    title: "Beach",
+    image: require("../assets/images/categories/beach.jpg")
+  },
+  {
+    title: "Camp",
+    image: require("../assets/images/categories/camp.jpg")
+  },
+  {
+    title: "Cities",
+    image: require("../assets/images/categories/cities.jpg")
+  },
+  {
+    title: "Desert",
+    image: require("../assets/images/categories/desert.jpg")
+  },
+  {
+    title: "Diving",
+    image: require("../assets/images/categories/diving.jpg")
+  },
+  {
+    title: "Forest",
+    image: require("../assets/images/categories/forest.jpg")
+  },
+  {
+    title: "Hiking",
+    image: require("../assets/images/categories/hiking.jpg")
+  },
+  {
+    title: "Landscape",
+    image: require("../assets/images/categories/landscape.jpg")
+  },
+  {
+    title: "Mountain",
+    image: require("../assets/images/categories/mountain.jpg")
+  },
+  {
+    title: "Ocean",
+    image: require("../assets/images/categories/ocean.jpg")
+  },
+  {
+    title: "Sunset",
+    image: require("../assets/images/categories/sunset.jpg")
+  },
+];
