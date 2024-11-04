f8c0f48fc7c05f19b70b056911d3bb5a6640db5d
Juan Sanchez <juanboca90@gmail.com>
2024-11-04 17:47:56 +0100

    Adding search bar, Categories and SortCategories components

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,19 +1,82 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import Categories from "@/components/Categories";
+import SortCategories from "@/components/SortCategories";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <View style={styles.titleContainer}>
-        <Text>Home Screen</Text>
-      </View>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Let's Explore</Text>
+          <Pressable onPress={() => console.log("press")}>
+            <Image
+              source={require("../../assets/images/avatar1.jpeg")}
+              style={styles.userImg}
+            />
+          </Pressable>
+        </View>
+
+        <View style={styles.searchBar}>
+          <MagnifyingGlassIcon size={20} strokeWidth={3} color="gray" />
+          <TextInput
+            placeholder="Type a destination"
+            placeholderTextColor="gray"
+            style={styles.searchInput}
+          />
+        </View>
+
+          <Categories />
+          <SortCategories />
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+  },
   titleContainer: {
     flexDirection: "row",
     alignItems: "center",
-    gap: 8,
+    justifyContent: "space-between",
+    marginVertical: 20
+  },
+  title: {
+    fontSize: hp(3.5),
+    fontWeight: "bold",
+    letterSpacing: 1,
+  },
+  userImg: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    backgroundColor: "#333",
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    backgroundColor: "#ddd",
+    borderRadius: 30,
+    marginVertical: 20,
+  },
+  searchInput: {
+    paddingLeft: 10,
+    fontSize: hp(2),
   },
 });
