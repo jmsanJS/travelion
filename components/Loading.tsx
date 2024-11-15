3056c984c31034fd531454970f8aeab1be3e9577
Juan Sanchez <juanboca90@gmail.com>
2024-11-15 17:09:57 +0100

    Loading component update

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -8,7 +8,7 @@ export default function Loading({ size }: LoadingProps) {
   return (
     <View>
       <LottieView
-        style={{ height: size }}
+        style={{ height: size, marginVertical: -40 }}
         source={require("../assets/images/loading.json")}
         autoPlay
         loop
