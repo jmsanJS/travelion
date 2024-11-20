63117dd5fa99ee4389afe9062f82535690ee8b3b
Juan Sanchez <juanboca90@gmail.com>
2024-11-20 14:43:31 +0100

    Adding user's account tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,12 @@
-import { Tabs } from "expo-router";
+import { Tabs, useSegments } from "expo-router";
 import React from "react";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 export default function TabLayout() {
+  const segments = useSegments();
+  const hideTabBar = segments[1] === "account";
+
   return (
     <Tabs
       screenOptions={{
@@ -20,6 +23,21 @@ export default function TabLayout() {
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
               name={focused ? "compass" : "compass-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarStyle: {
+            display: hideTabBar ? "none" : "flex",
+          },
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "person-circle" : "person-circle-outline"}
               color={color}
             />
           ),
