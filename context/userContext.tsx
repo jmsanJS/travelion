f030bf64dc00b8d3a9d46064cef718e287d72873
Juan Sanchez <juanboca90@gmail.com>
2024-11-22 11:29:19 +0100

    Adding AuthState from Firebase and isAuthenticated property

--- context/userContext.tsx
+++ context/userContext.tsx
@@ -1,25 +1,19 @@
-import React, { createContext, useContext, useState } from "react";
+import { auth, db } from "@/firebaseConfig";
+import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface User {
+  username: string | undefined;
+  email: string | undefined;
+  pictureUrl: string | undefined;
+  uid: string | undefined;
+}
 
 interface UserContextType {
-  user:
-    | {
-        username: string | undefined;
-        email: string | undefined;
-        pictureUrl: string | undefined;
-        uid: string | undefined;
-      }
-    | undefined | null;
-  setUser: React.Dispatch<
-    React.SetStateAction<
-      | {
-          username: string | undefined;
-          email: string | undefined;
-          pictureUrl: string | undefined;
-          uid: string | undefined;
-        }
-      | undefined | null
-    >
-  >;
+  user: User | null | undefined;
+  setUser: React.Dispatch<React.SetStateAction<User | null | undefined>>;
+  isAuthenticated: boolean | undefined;
 }
 
 const UserContext = createContext<UserContextType | undefined>(undefined);
@@ -27,18 +21,45 @@ const UserContext = createContext<UserContextType | undefined>(undefined);
 export const UserContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [user, setUser] = useState<
-    | {
-        username: string | undefined;
-        email: string | undefined;
-        pictureUrl: string | undefined;
-        uid: string | undefined;
+  const [user, setUser] = useState<User | null | undefined>(undefined);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | undefined>(undefined);
+
+  // Actualizar los datos del usuario desde Firestore
+  const updateUserData = async (userId: string) => {
+    try {
+      const refDoc = doc(db, "users", userId);
+      const docSnapshot = await getDoc(refDoc);
+
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        setUser({
+          username: data.username,
+          email: data.email,
+          pictureUrl: data.profileUrl,
+          uid: userId,
+        });
       }
-    | undefined | null
-  >(undefined);
+    } catch (error) {
+      console.error("Error fetching user data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userFromDb: FirebaseUser | null) => {
+      if (userFromDb) {
+        setIsAuthenticated(true);
+        updateUserData(userFromDb.uid);
+      } else {
+        setIsAuthenticated(false);
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, isAuthenticated }}>
       {children}
     </UserContext.Provider>
   );
@@ -47,9 +68,7 @@ export const UserContextProvider: React.FC<{ children: React.ReactNode }> = ({
 export const useUser = () => {
   const context = useContext(UserContext);
   if (!context) {
-    throw new Error(
-      "useUser must be used within UserContextProvider"
-    );
+    throw new Error("useUser must be used within UserContextProvider");
   }
   return context;
 };
